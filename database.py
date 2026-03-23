106eb58be8bbb09f90cce2075e1200a4bf9519e5
Mark McKee <mark_a_mckee@hotmail.com>
2026-03-23 16:31:11 +0000

    improving environment handling

--- database.py
+++ database.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-
-DATABASE_URL = "postgresql://payments:password@localhost/payments_db"
+from dotenv import load_dotenv
+import os
+load_dotenv()
+   
+DATABASE_URL = os.environ.get("DATABASE_URL")
+#DATABASE_URL = "postgresql://payments:password@localhost/payments_db"
 
 engine = create_engine(DATABASE_URL)
 
